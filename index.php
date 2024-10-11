8c08cae91ba2080dca30501a168eaee993bdc33e
shifaNA25266 <nanamiiw255@gmail.com>
2024-10-11 14:15:03 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+include "koneksi.php";
+$sql = "SELECT * FROM notes ORDER BY created_at DESC";
+$result = $conn->query($sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Catatan</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <h2> <strong>Daftar Catatan</strong></h2>
+    <a class="btn btn-secondary" href="pages/create.php">Tambah Catatan Baru</a>
+    <br><br>
+    <table class="table table-striped" border="1" cellpadding="8" cellspacing="0" >
+        <thead>
+            <tr class="table-primary">
+                <th>No</th>
+                <th>Judul</th>
+                <th>Isi Catatan</th>
+                <th>Tanggal Dibuat</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            if ($result->num_rows > 0) {
+                $no = 1;
+                // Looping data dari database
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $no++ . "</td>";
+                    echo "<td>" . $row['judul'] . "</td>";
+                    echo "<td>" . $row['isi'] . "</td>";
+                    echo "<td>" . date('d-m-Y H:i', strtotime($row['created_at'])) . "</td>";
+                    echo "<td>";
+                    echo "<a href='./pages/edit.php?id=" . $row['id'] . "'>Edit</a> | ";
+                    echo "<a href='./actions/delete.php?id=" . $row['id'] . "' onclick='return confirm(\"Apakah anda yakin ingin menghapus catatan ini?\");'>Hapus</a>";
+                    echo "</td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "<tr><td colspan='5'>Belum ada catatan.</td></tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
+<?php
+$conn->close();
+?>
